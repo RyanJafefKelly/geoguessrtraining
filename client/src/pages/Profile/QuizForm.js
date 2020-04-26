22bac31cca0deb39683009fcfee59819fe475efd
RyanJafefKelly <ryan@kiiii.com>
2020-04-26 13:31:56 +1000

    add quiz form updates

--- client/src/pages/Profile/QuizForm.js
+++ client/src/pages/Profile/QuizForm.js
@@ -1,15 +1,57 @@
 import React from 'react';
-import { Formik } from 'formik';
+import { Formik, Field } from 'formik';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { Typography } from '@material-ui/core';
+//add question on button click on form
+function FormFields(index) {
+    return(
+        <React.Fragment>
+<div>
+                    <Field type="text" name={`questionText${index.index}`} placeholder="Question"    />
+                    {/* </div>
+                    <div> */}
+                    <Field type="text" name={`imageUrl${index.index}`} placeholder="Image URL" />
+                    {/* </div>
+                    <div> */}
+                    <Field type="text" name={`correctAnswer${index.index}`} placeholder="Answer" />
+                    </div>
+        </React.Fragment>
+    )
+}
 
 export default function QuizForm() {
+    const [numQuestions, setNumQuestions] = React.useState(1);
     return (
         <React.Fragment>
-            <Formik>
-          <form>
-              <input type="email" name="email" />
-          </form>
+            <div>
+            <h1>My Form</h1>
+            <Formik
+                initialValues={{questionText0: '', imageUrl0: '', correctAnswer0: '', 
+                questionText1: '', imageUrl1: '', correctAnswer1: '',
+                questionText2: '', imageUrl2: '', correctAnswer2: '',
+                questionText3: '', imageUrl3: '', correctAnswer3: '',
+                questionText4: '', imageUrl4: '', correctAnswer4: ''}}
+                onSubmit={(values, actions) => {
+                    setTimeout(() => {
+                      alert(JSON.stringify(values, null, 2));
+                      actions.setSubmitting(false);
+                    }, 1000);
+                  }}
+            >
+                { props => (
+                <React.Fragment>
+                <form onSubmit={props.handleSubmit}>
+                    {Array(numQuestions).fill().map((num, index) => {
+                        return <div>Question: {index + 1} <FormFields index={index} /></div>
+                })}
+                </form>
+                <Button onClick={() => setNumQuestions(numQuestions + 1)}>Add Question</Button>
+                <Button onClick={() => props.handleSubmit()}>Submit</Button>       
+                </React.Fragment>         
+                                )}
             </Formik>
-
+            </div>
         </React.Fragment>
     )
 }
--- client/src/pages/Profile/index.js
+++ client/src/pages/Profile/index.js
@@ -3,9 +3,30 @@ import QuizForm from './QuizForm';
 // import { Alert, Badge, Button, Card, CardBody, CardHeader, Col, Modal, ModalHeader, Row, Table, ModalBody, ModalFooter } from 'reactstrap';
 import Modal from '@material-ui/core/Modal';
 import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
+const useStyles = makeStyles((theme) => ({
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    //   width: 1000
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+  }));
+  
 
 export default function Profile() {
     const [modal, setModal] = useState(false);
+    const classes = useStyles();
+
     // const [success, setSuccess] =useState(false);
     // const [data, setData] = useState({
     //   title: null, description: null, startDate: null, endDate: null,
@@ -28,8 +49,23 @@ export default function Profile() {
       }
     return (
         <div>
-            <Modal open={modal} onClose={() => setModal(false)}> 
-                <QuizForm />
+            <Modal 
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={modal} 
+                onClose={() => setModal(false)}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                  timeout: 500,
+                }}
+                > 
+                <Fade in={modal}>
+                    <div className={classes.paper}>
+                    <QuizForm />
+                    </div>
+                </Fade>
             </Modal>
             <Button color="primary" onClick={toggle}>Add Quiz</Button>     
         </div>
