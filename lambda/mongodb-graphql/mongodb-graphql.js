dc2d0f92287ac287359240b6e48b3c2dcf386f68
RyanJafefKelly <ryan@kiiii.com>
2020-05-02 15:05:39 +1000

    log URI

--- lambda/mongodb-graphql/mongodb-graphql.js
+++ lambda/mongodb-graphql/mongodb-graphql.js
@@ -3,9 +3,11 @@ const typeDefs = require("./typedefs");
 const resolvers = require("./resolvers");
 const connectToMongoDB = require("./db");
 require('encoding');
+const URI = process.env.DB_URI;
 
 
 exports.handler = async function(event, context) {
+  console.log(URI);
   const db = await connectToMongoDB();
   const server = new ApolloServer({
     typeDefs,
