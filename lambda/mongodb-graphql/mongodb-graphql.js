36c99e95f45eeacef3d36d068addd586a77a691b
RyanJafefKelly <ryan@kiiii.com>
2020-05-09 14:50:23 +1000

    server updates

--- lambda/mongodb-graphql/mongodb-graphql.js
+++ lambda/mongodb-graphql/mongodb-graphql.js
@@ -5,23 +5,34 @@ const connectToMongoDB = require("./db");
 require('encoding');
 const URI = process.env.DB_URI;
 
-exports.handler = async function(event, context) {
-  console.log(URI);
-  const db = await connectToMongoDB();
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers: resolvers(db),
-    context: ({ event, context }) => ({
-      headers: event.headers,
-      functionName: context.functionName,
-      event,
-      context,
-    }),
-    playground: {
-      endpoint: "/dev/graphql"
-    }
-  });
-  return server.createHandler();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: true
+});
+
+exports.handler = server.createHandler();
+
+
+// exports.handler = async function(event, context) {
+//   console.log(URI);
+//   const db = await connectToMongoDB();
+//   const server = new ApolloServer({
+//     typeDefs,
+//     resolvers: resolvers(db),
+//     context: ({ event, context }) => ({
+//       headers: event.headers,
+//       functionName: context.functionName,
+//       event,
+//       context,
+//     }),
+//     playground: {
+//       endpoint: "/dev/graphql"
+//     }
+//   });
+//   return server.createHandler();
   // return new Promise((yay, nay) => {
   //   const cb = (err, args) => (err ? nay(err) : yay(args));
   //   server.createHandler()(event, context, cb);
